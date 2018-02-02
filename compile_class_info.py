d23ae007991e12a302ed1e114a1129c45007b5ba
Harvey Shi <harveyyshi@gmail.com>
2018-02-02 18:33:32 -0500

    Fixes JSON format and comments. - The JSON files were previously doubly processed via json.dumps(). - Also reformats function comment positions. (aiming at PyDoc compatibility one day)

--- compile_class_info.py
+++ compile_class_info.py
@@ -1,5 +1,5 @@
 import glob
-import os # for getting just the name of the file
+import os      # for getting just the name of the file
 import re
 import json
 
@@ -11,20 +11,21 @@ def main():
     write_csv(student_data)
     write_json(student_data)
 
-'''
-gets all the csv files in ./$dir
-'''
+
 def collect_csv_files(dir):
+    '''
+    gets all the csv files in ./$dir
+    '''
     return glob.glob(dir + '*.csv')
 
 
-'''
-given path to file, returns contents minus whitespace, empty lines and comments (#)
-'''
-def get_file_contents(filename):
 
-    with file(filename) as f:
-        cleaned_file = '' # will store cleaned up version of file
+def get_file_contents(filename):
+    '''
+    given path to file, returns contents minus whitespace, empty lines and comments (#)
+    '''
+    with open(filename) as f:
+        cleaned_file = '' # to store cleaned up version of file
         for line in f:
             ls = line.strip() # remove whitespace
 
@@ -35,19 +36,17 @@ def get_file_contents(filename):
         return cleaned_file
 
 
-'''
-concatenates file contents into a dictionary keyed by netID
-'''
 def cat_data(filenames):
+    '''
+    concatenates file contents into a dictionary keyed by netID
+    '''
 
     student_data = {} # will eventually contain all the csv student data
     num_camel_case = 0
     unique_teams = []
 
     for fn in filenames:
-        # filename without extension or directory
-        # uses solution from
-        # https://stackoverflow.com/questions/678236/how-to-get-the-filename-without-the-extension-from-a-path-in-python
+        # get name of file without extension or directory
         name = os.path.basename(fn).rsplit('.', 1)[0]
 
         # confirm that name is valid
@@ -75,17 +74,18 @@ def cat_data(filenames):
     print('Number of team names using CamelCase: {}'.format(num_camel_case))
     return student_data
 
-'''
-returns True if $string has no spaces (internal)
-'''
+
 def check_no_spaces(string):
+    '''
+    returns True if @string has no spaces (internal)
+    '''
     return ' ' not in string.strip()
 
-'''
-returns True if $string is in CamelCase
-'''
 
 def is_camel_case(string):
+    '''
+    returns True if @string is in CamelCase
+    '''
     # split the string at capital letters only
     # regex derived from https://stackoverflow.com/questions/2277352/split-a-string-at-uppercase-letters
     camel_humps = re.findall('[A-Z][^A-Z]*', string.strip())
@@ -95,19 +95,20 @@ def is_camel_case(string):
             return False
     return True
 
-'''
-writes a single csv given array of entries
-'''
 
 
 def write_csv(student_data):
+    '''
+    writes a single csv given array of entries
+    '''
     with open('everyone.csv', 'w') as f:
         f.write('\n'.join(student_data.values()))
 
-'''
-writes a single json per student
-'''
+
 def write_json(student_data):
+    '''
+    writes a single json per student
+    '''
     for netID, data in student_data.items():
         allData = [item.strip() for item in data.split(',')]
 
@@ -125,7 +126,7 @@ def write_json(student_data):
 
         # write to file
         with open(json_file_path, 'w') as f:
-            f.write(json.dumps(json_version, indent=4, sort_keys=False))
+            f.write(json_version)
 
 if __name__ == "__main__":
     main()
